70ba731e0cbaf58c958648c3f570b80148c02b24
ViniWei <viniciusweigert152@gmail.com>
2024-12-31 15:04:10 -0300

    Emacs change location of temporary files

--- elisp/options.el
+++ elisp/options.el
@@ -18,6 +18,15 @@
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+;Keep my folders clean
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+
+(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+
+(setq projectile-known-projects-file (expand-file-name "tmp/projectile-bookmarks.eld" user-emacs-directory)
+      lsp-session-file (expand-file-name "tmp/.lsp-session-v1" user-emacs-directory))
+
 ; default hooks
 (setq display-line-numbers-type 'relative) 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
