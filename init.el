fc01a4d1bb79134e9da7c7a47c38c3cd343be8de
ViniWei <viniciusweigert152@gmail.com>
2025-01-02 21:56:55 -0300

    Make exec-path-from-shell not execute on windows

--- init.el
+++ init.el
@@ -17,9 +17,10 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package exec-path-from-shell
-  :config
-  (exec-path-from-shell-initialize))
+(when (not (string-equal system-type "windows-nt"))
+  (use-package exec-path-from-shell
+    :config
+    (exec-path-from-shell-initialize)))
 
 (load-file "~/.emacs.d/elisp/completion.el")
 (load-file "~/.emacs.d/elisp/visual.el")
